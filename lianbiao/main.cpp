4441e7ac23b185037b1d8bc36b4c1dd3b14ea2b0
s20151104683 <s20151104683@mac25.local>
2016-10-17 19:42:36 +0800

    Initial Commit

---
+++ lianbiao/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lianbiao
+//
+//  Created by s20151104683 on 16/10/17.
+//  Copyright © 2016年 s20151104683. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
